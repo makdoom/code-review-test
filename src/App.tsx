51a63bf27a7e6b47648a89657446f4d1b0b4b6e3
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-22 16:30:02 +0530

    Change console log message for testing

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("testing - 1");
+  console.log("testing - 2");
   return (
     <>
       <section id="center">
