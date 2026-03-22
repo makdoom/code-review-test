838086f44640ee69d598c6c4631a1d31cb2afcc6
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-22 16:40:44 +0530

    Update console log message in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
-  console.log("testing - 4");
+  console.log("testing - 5");
   return (
     <>
       <section id="center">
