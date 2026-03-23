bdd6cc32c41fc78e82ee503b9f3a3c0a682b99da
Makdoom Shaikh <makshaikh99@gmail.com>
2026-03-23 22:57:46 +0530

    feat: Update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ let users = [];
 function App() {
   const [count, setCount] = useState(0);
 
-  let admin = users.find((item) => item.role == "designer");
+  let admin = users.find((item) => item.role == "dev");
   if (admin.status == "1") {
     return <p>Admin</p>;
   }
